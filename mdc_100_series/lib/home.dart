b0ed1a5ae44598e5577336960b07c384cf219f9f
Dreamwalker <aristojeff@gmail.com>
2018-08-12 01:44:09 +0900

    add card

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -25,11 +25,104 @@ class HomePage extends StatelessWidget {
       // TODO: Add app bar (102)
       appBar: AppBar(
         title: Text('SHRINE'),
+
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: (){
+            print('Menu Button Pressed');
+          },
+        ),
+
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: (){
+              print('Search Button');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: (){
+              print('Filter Clicked');
+            },
+          )
+
+        ],
+
       ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0/9.0,
+        children: <Widget>[
+          Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 18.0 / 11.0,
+                  child: Image.asset('assets/diamond.png'),
+                ),
+                new Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0,8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Title'),
+                      SizedBox(height: 8.0),
+                      Text('secondary Text'),
+                    ],
+                  ),)
+              ],
+            ),
+          )
+        ],
+      ),
+
       // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
-      ),
+//      body: Center(
+//        child: Text('You did it!'),
+//      ),
     );
   }
+}
+
+// TODO: Make a collection of cards (102)
+List<Card> _buildGridCards(int count) {
+  List<Card> cards = List.generate(
+    count,
+        (int index) => Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 18.0 / 11.0,
+            child: Image.asset('assets/diamond.png'),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Title'),
+                SizedBox(height: 8.0),
+                Text('Secondary Text'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+
+  return cards;
 }
