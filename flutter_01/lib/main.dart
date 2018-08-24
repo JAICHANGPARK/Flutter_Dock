cccad40c90ceeefc8e649fdc5da374f9fc2c455a
Dreamwalker <aristojeff@gmail.com>
2018-08-24 22:50:24 +0900

    Flutter Button Click

--- flutter_01/lib/main.dart
+++ flutter_01/lib/main.dart
@@ -7,19 +7,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return MaterialApp(
-        title: 'My App Hello world',
-        home: Scaffold(
-          appBar: AppBar(title: Text('Flutter App')),
-          body: Center(
-            child: new Text(
-              'Hello world',
-              style: new TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
-            ),
+    return MaterialApp(title: 'My App Hello world',
+        home: ClickMyApp());
+  }
+}
+
+class ClickMyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ChangeText();
+}
+
+class ChangeText extends State<ClickMyApp> {
+  String defaultText = 'Hello ! Click me to change content';
+
+  int i = 0;
+
+  void changeText() {
+    setState(() {
+      if (i % 2 == 0) {
+        defaultText = 'Text hasbeen changed';
+      } else {
+        defaultText = 'Hello! click me to change content';
+      }
+      i++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter App')),
+      body: Center(
+        child: new Text(
+          defaultText,
+          style: new TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
           ),
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeText,
+        tooltip: 'Click Me',
+        child: Icon(Icons.mouse),
+      ),
+    );
   }
 }
