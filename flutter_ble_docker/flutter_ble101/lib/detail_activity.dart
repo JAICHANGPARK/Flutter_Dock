c103873e921f90be4f85418e9b87403292e10757
Dreamwalker <aristojeff@gmail.com>
2018-09-25 01:48:29 +0900

    이제 데이터 처리가 문제네요
    1. 쓰기 과정에서 문자열을 입력했을때
    리스트로 변경하는게 필요합니다.

--- flutter_ble_docker/flutter_ble101/lib/detail_activity.dart
+++ flutter_ble_docker/flutter_ble101/lib/detail_activity.dart
@@ -4,17 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter_ble101/widgets.dart';
 
-class DetailScreen extends StatefulWidget{
+class DetailScreen extends StatefulWidget {
   BluetoothDevice device;
+
   DetailScreen(this.device);
 
   @override
   State<StatefulWidget> createState() => DetailScreenState(device);
 }
 
-class DetailScreenState extends State<DetailScreen>{
-
+class DetailScreenState extends State<DetailScreen> {
   DetailScreenState(this.device);
+
   FlutterBlue _flutterBlue = FlutterBlue.instance;
 
   /// Scanning
@@ -28,12 +29,18 @@ class DetailScreenState extends State<DetailScreen>{
 
   /// Device
   BluetoothDevice device;
+
   bool get isConnected => (device != null);
   StreamSubscription deviceConnection;
   StreamSubscription deviceStateSubscription;
   List<BluetoothService> services = new List();
   Map<Guid, StreamSubscription> valueChangedSubscriptions = {};
   BluetoothDeviceState deviceState = BluetoothDeviceState.disconnected;
+
+  static final TextEditingController myControllers = new TextEditingController();
+
+  static List<int> mySendValue = new List();
+
 
   @override
   void initState() {
@@ -44,19 +51,51 @@ class DetailScreenState extends State<DetailScreen>{
     super.initState();
   }
 
+  static _handleSubmission(String text) {
+// Not triggered when you press enter on keyboard in  android simulator
+// Triggers if you tap on the Done button.
+
+    String value = myControllers.text;
+    print(text.length);
+
+    for (int i = 0; i<text.length; i++){
+      print(text[i]);
+      int tmp = int.parse(text[i]);
+      print(tmp);
+//          mySendValue.add(int.parse(value[i]));
+    }
+
+
+  }
+
+  Widget inputEditText = new Padding(
+    padding: EdgeInsets.all(16.0),
+    child: new TextField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
+          hintText: "input Text"),
+      controller: myControllers,
+      onEditingComplete: () {
+        print(myControllers.text);
+      },
+      onSubmitted: _handleSubmission,
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-
     var tiles = new List<Widget>();
 
     tiles.add(_buildDeviceStateTile());
+    tiles.add(inputEditText);
     tiles.addAll(_buildServiceTiles());
-
 
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text("Detail Device"),
-      backgroundColor: Colors.amber,),
+      appBar: AppBar(
+        title: Text("Detail Device"),
+        backgroundColor: Colors.amber,
+      ),
       body: new ListView(
         children: tiles,
       ),
@@ -68,11 +107,10 @@ class DetailScreenState extends State<DetailScreen>{
     setState(() {
       deviceState = state;
       print('State refreshed: $deviceState');
-      if(deviceState == BluetoothDeviceState.disconnected){
+      if (deviceState == BluetoothDeviceState.disconnected) {
         _connect(d);
       }
     });
-
   }
 
   _buildDeviceStateTile() {
@@ -95,9 +133,9 @@ class DetailScreenState extends State<DetailScreen>{
     deviceConnection = _flutterBlue
         .connect(device, timeout: const Duration(seconds: 4))
         .listen(
-      null,
-      onDone: _disconnect,
-    );
+          null,
+          onDone: _disconnect,
+        );
 
     // Update the connection state immediately
     device.state.then((s) {
@@ -136,28 +174,72 @@ class DetailScreenState extends State<DetailScreen>{
     });
   }
 
-  List<Widget> _buildServiceTiles() {
+  _readDescriptor(BluetoothDescriptor d) async {
+    await device.readDescriptor(d);
+    setState(() {});
+  }
 
-    return services.map((s) => new ServiceTile(
-      service: s, characteristicTiles:
-      s.characteristics.map((c) => new CharacteristicTile(
-        characteristic:  c,
-      onReadPressed: () async{
-        List<int> received = await device.readCharacteristic(c);
-        setState(() {
-          for(int i = 0; i < received.length ; i++){
-            print(received[i]);
-          }
-        });
-      },
-      onWritePressed: (){},
-      onNotificationPressed: (){},
-      descriptorTiles: c.descriptors.map((d) => new DescriptorTile(
-        descriptor: d,
-        onReadPressed: (){},
-        onWritePressed: (){},)).toList(),
-      )).toList(),
-    )).toList();
+  _writeDescriptor(BluetoothDescriptor d) async {
+    await device.writeDescriptor(d, [0x12, 0x34]);
+    setState(() {});
+  }
+
+  List<Widget> _buildServiceTiles() {
+    return services
+        .map((s) => new ServiceTile(
+              service: s,
+              characteristicTiles: s.characteristics
+                  .map((c) => new CharacteristicTile(
+                        characteristic: c,
+                        onReadPressed: () async {
+                          List<int> received =
+                              await device.readCharacteristic(c);
+                          setState(() {
+                            BluetoothCharacteristic tmp = c;
+
+                            print('char -->  $c');
+                            print(c.uuid);
+
+//          for(int i = 0; i < received.length ; i++){
+//            print(received[i]);
+//          }
+                          });
+                        },
+                        onWritePressed: () async {
+                          await device.writeCharacteristic(c, mySendValue, type: CharacteristicWriteType.withResponse);
+                          setState(() {});
+                        },
+                        onNotificationPressed: () async {
+                          print(c.uuid);
+                          if (c.isNotifying) {
+                            await device.setNotifyValue(c, false);
+                            // Cancel subscription
+                            valueChangedSubscriptions[c.uuid]?.cancel();
+                            valueChangedSubscriptions.remove(c.uuid);
+                          } else {
+                            await device.setNotifyValue(c, true);
+                            // ignore: cancel_subscriptions
+                            final sub = device.onValueChanged(c).listen((d) {
+                              setState(() {
+                                print('onValueChanged $d');
+                              });
+                            });
+                            // Add to map
+                            valueChangedSubscriptions[c.uuid] = sub;
+                          }
+                          setState(() {});
+                        },
+                        descriptorTiles: c.descriptors
+                            .map((d) => new DescriptorTile(
+                                  descriptor: d,
+                                  onReadPressed: () => _readDescriptor,
+                                  onWritePressed: () => _writeDescriptor,
+                                ))
+                            .toList(),
+                      ))
+                  .toList(),
+            ))
+        .toList();
   }
 
   @override
@@ -173,5 +255,4 @@ class DetailScreenState extends State<DetailScreen>{
     _disconnect();
     super.dispose();
   }
-
 }
