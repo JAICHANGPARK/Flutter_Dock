3b646a1c421f04e43b76df77cab96c1e6b8253af
Dreamwalker <aristojeff@gmail.com>
2018-11-18 01:13:33 +0900

    a
    a

---
+++ flutter_bmi/lib/widgets/card_title.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CardTitle extends StatelessWidget {
+  final String title;
+
+  const CardTitle(this.title, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+    );
+
+
+  }
+}
