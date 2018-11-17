80c5d53b079d1d486995afb94fd1c41f2d4b4ac0
Dreamwalker <aristojeff@gmail.com>
2018-11-18 01:24:08 +0900

    draw circle

--- flutter_bmi/lib/widgets/gender_card.dart
+++ flutter_bmi/lib/widgets/gender_card.dart
@@ -23,15 +23,54 @@ class _GenderCardState extends State<GenderCard> {
       child: SizedBox(
         width: double.infinity,
         child: Padding(
-            padding: EdgeInsets.only(top: screenAwareSize(8.0, context)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                CardTitle("Gender")
-              ],
-            ),
+          padding: EdgeInsets.only(top: screenAwareSize(8.0, context)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CardTitle("Gender"),
+              Padding(
+                padding: EdgeInsets.only(top: screenAwareSize(16.0, context)),
+                child: _drawMainStack(),
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _drawMainStack() {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        _drawCircleIndicator(),
+      ],
+    );
+  }
+
+  Widget _drawCircleIndicator() {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        GenderCircle(),
+      ],
+    );
+  }
+}
+
+class GenderCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      width: _circleSize(context),
+      height: _circleSize(context),
+      
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color.fromRGBO(244, 244, 244, 1.0),
+      ),
+      
     );
   }
 }
